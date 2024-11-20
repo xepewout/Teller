13afd51c936e9240d780506b3bcc69a42a47cdb9
xepewout <111144938+xepewout@users.noreply.github.com>
2024-11-20 00:27:10 -0800

    version .002
    -messed around with flipping faces

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,7 +3,6 @@ extends CharacterBody3D
 var speed = .000005
 var movementSpeed = .2
 var target_velocity = Vector3.ZERO
-@export var fall_acceleration = 75
 @onready var death_sensor: RayCast3D = $DeathSensor
 
 func _physics_process(delta: float) -> void:
@@ -19,17 +18,9 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("move_down"):
 		direction.y -= 1
 
-	#if direction != Vector3.ZERO:
-		#direction = direction.normalized()
-		#$Pivot.basis = Basis.looking_at(direction)
-
 	# Ground Velocity
 	target_velocity.x = direction.x * movementSpeed
 	target_velocity.y = direction.y * movementSpeed
-
-	# Vertical Velocity
-	#if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
-		#target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 
 	# Moving the Character
 	velocity = target_velocity
